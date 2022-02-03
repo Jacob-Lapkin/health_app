a32519246ef08c49ed926102a4c5fd8db2f063ac
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-02-03 13:18:57 -0800

    ignore
    gitignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jacoblapkin/Documents/GitHub/health_app/backend/venv"
+set -gx VIRTUAL_ENV "/Users/jacoblapkin/Documents/GitHub/health_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
